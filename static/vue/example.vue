63b1cbe4ea9a349282de38d7c612895f350312c5
louis_dresse <louisdresse@gmail.com>
2025-11-15 17:33:53 +0100

    added Mews documentation (postman requests and responses previously tested + vue example template)

---
+++ static/vue/example.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>Example Component</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExampleComponent',
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 20px;
+}
+
+h1 {
+  color: #333;
+}
+
+p {
+  color: #666;
+}
+</style>
